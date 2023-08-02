9384bcbc123ec80f09de4174ace6d12f5454306b
daisytorres <daisytorres.cal@gmail.com>
2023-08-02 12:47:01 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard lastName = "Heron" firstName = "Cady" age = {18} hairColor = "Red"  />
+      <PersonCard lastName = "George" firstName = "Regina" age = {88} hairColor = "Blonde"/>
+      <PersonCard lastName = "Smith" firstName = "Karen" age = {19} hairColor = "Blonde"/>
+      <PersonCard lastName = "Wienders" firstName = "Gretchen" age = {21} hairColor = "Brown"/>
     </div>
   );
 }
